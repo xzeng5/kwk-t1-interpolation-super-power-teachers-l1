563baa1f3eacbe629be41e8db69b5e56d388d07c
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-05-29 18:23:55 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
 # Write your #display_rainbow method here
+color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+def display_rainbow(list)
+  puts "R: #{list[0]}, O: #{list[1]}, Y: #{list[2]}, G: #{list[3]}, B: #{list[4]}, I: #{list[5]}, V: #{list[6]}"
+end
+
+display_rainbwo(color)
