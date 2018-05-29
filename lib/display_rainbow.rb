66c595cbece5722f494c011985b5ec15871c35aa
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-05-29 18:24:07 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,4 +5,4 @@ def display_rainbow(list)
   puts "R: #{list[0]}, O: #{list[1]}, Y: #{list[2]}, G: #{list[3]}, B: #{list[4]}, I: #{list[5]}, V: #{list[6]}"
 end
 
-display_rainbwo(color)
+display_rainbow(color)
